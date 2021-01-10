6ea135e648de73369a3828769fd7b3eff06660b3
LEEHyokyun <gyrbs22@gmail.com>
2021-01-10 16:25:10 +0900

    indicating i and j

--- week3/01_bubble_sort.py
+++ week3/01_bubble_sort.py
@@ -6,10 +6,11 @@ input = [4, 6, 2, 9, 1]
 def bubble_sort(array):
     n = len(array)
     for i in range(n - 1):
+        print("i is", i)
         for j in range(n - i - 1):  # i만큼 반복하면서 이미 정렬, i 제외
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j] #순서 바꾸는 명령어
-            print(j)
+            print("J is", j)
     return
 
 
