84101df4347e6f8eddc6b1051c555aacb983246a
hyokyun <gyrbs22@gmail.com>
2021-04-24 10:41:13 +0900

    Update 03_insertion_sort.py

--- week3/03_insertion_sort.py
+++ week3/03_insertion_sort.py
@@ -7,12 +7,15 @@ input = [4, 6, 2, 9, 1]
 def insertion_sort(array):
     n = len(array)
     for i in range(1, n):  # 숫자 6부터시작, 배열크기
+        print("i is", i)
         for j in range(i):
+            print("j is", j)
             if array[i - j - 1] > array[i - j]:
                 array[i - j - 1], array[i - j] = array[i - j], array[i - j - 1]
+                print(array)
             else:
-                break #반복문종료(정렬되어있는데 그 숫자보다도 크면 정렬할 이유가 없다)
-                      #for문으로 올라감??
+                break # 반복문종료(정렬되어있는데 그 숫자보다도 크면 정렬할 이유가 없다)
+                      # for문으로 올라감??
     return
 
 
